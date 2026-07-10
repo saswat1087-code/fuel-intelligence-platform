54dcca5de11f2b39faae1bc1685ea01a15313880
saswat1087-code <saswat1087@gmail.com>
2026-07-10 19:47:52 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import PredictionForm from './components/PredictionForm.tsx';
+import PredictionForm from './components/PredictionForm';
 
 // Create a theme
 const theme = createTheme({
